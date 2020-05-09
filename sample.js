82b701409c24e2923d291b2e2654b80bc878da86
MichiMar <michimar.alexis@gmail.com>
2020-05-08 22:03:34 -0500

    JS snippets added

---
+++ sample.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class MyReactComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from Tips and trcks for Efficiently Working with VS Code</h1>
+      </div>
+    );
+  }
+}
